ccdd5189287c67343dd89905e39e91975d093780
nxdal02 <nedalt48@gmail.com>
2024-03-19 22:16:08 +0000

    add car 2.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+function CarList() {
+   // State variables
+   const [cars, setCars] = useState([]);
+   const [newCar, setNewCar] = useState({ make: '', model: '', bodyType: '', seats: 0, price: 0 });
+  function fetchCars() {
+    axios.get('http://localhost:8080/api/v1/car/all')
+      .then(response => {
+        console.log(response.data); // for debug
+        setCars(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching cars:', error);
+      });
+  }
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setNewCar({ ...newCar, [name]: value });
+  }
+
+  function addCar() {
+    console.log(newCar);
+    axios.post('http://localhost:8080/api/v1/car/add', newCar)
+      .then(() => {
+        // After adding the new car, fetch the updated list of cars
+        fetchCars();
+        // Reset the new car form
+        setNewCar({ make: '', model: '', bodyType: '', seats: 0, price: 0 });
+      })
+      .catch(error => {
+        console.error('Error adding car:', error);
+      });
+  }
+
+  // Fetch the list of cars when the component mounts
+  useEffect(() => {
+    fetchCars();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Cars</h1>
+      {/* New car form */}
+      <form onSubmit={addCar}>
+        <input type="text" name="make" placeholder="Make" value={newCar.make} onChange={handleInputChange} />
+        <input type="text" name="model" placeholder="Model" value={newCar.model} onChange={handleInputChange} />
+        <input type="text" name="bodyType" placeholder="Body Type" value={newCar.bodyType} onChange={handleInputChange} />
+        <input type="number" name="seats" placeholder="Seats" value={newCar.seats} onChange={handleInputChange} />
+        <input type="number" name="price" placeholder="Price" value={newCar.price} onChange={handleInputChange} />
+        <button type="submit">Add Car</button>
+      </form>
+      {/* Display the list of cars */}
+      <ul>
+        {cars.map((car) => (
+          <li key={car.id}>
+            {car.make} - {car.model} - {car.bodyType} - Seats: {car.seats} - Price: {car.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default CarList;
